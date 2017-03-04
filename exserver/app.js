b257eb361ce5568e23c775ae052ea937da8aa680
Rammohan Vadlamani <rmmohan@gmail.com>
2017-03-04 16:24:08 +0530

    Added body parsing logic

--- exserver/app.js
+++ exserver/app.js
@@ -1,7 +1,11 @@
 var express = require("express");
 var path = require("path");
+var bodyParser = require("body-parser");
 
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -21,6 +25,12 @@ app.get("/download", function(req, res) {
 
 app.get("/about", function(req, res) {
     res.redirect("/about.html");
+})
+
+app.post("/subscribe", function(req, res) {
+    var name = req.body.name;
+    var email = req.body.email;
+    console.log(name + " has subsribed with " + email);
 })
 
 app.listen(3000, function() {
