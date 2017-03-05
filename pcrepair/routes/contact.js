703bf3244a80e4726c4f12fc4d33ce47a943cdf5
Rammohan Vadlamani <rmmohan@gmail.com>
2017-03-05 11:22:10 +0530

    removed some unnecessary files

---
+++ pcrepair/routes/contact.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+
+router.get('/', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
+});
+
+router.post('/sendMail', function(req, res) {
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+          user: 'rmmohan@gmail.com',
+          pass: 'h*a*c*k*e*r*@123'
+      }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+      from: '"Fred Foo" <foo@blurdybloop.com>', // sender address
+      to: 'bar@blurdybloop.com, baz@blurdybloop.com', // list of receivers
+      subject: 'Hello ✔', // Subject line
+      text: 'Hello world ?', // plain text body
+      html: '<b>Hello world ?</b>' // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
+      console.log('Message %s sent: %s', info.messageId, info.response);
+  });
+});
+
+module.exports = router;
